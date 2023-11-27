0a1de48dcc0d9e970025a779cff3ac930f3e2638
AbuTalha3 <127955219+AbuTalha3@users.noreply.github.com>
2023-11-28 03:18:38 +0500

    Deleting comments

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -1,49 +1,19 @@
-class ArticlesController < ApplicationController
-  def index
-    @articles = Article.all
-  end
-
-  def show
-    @article = Article.find(params[:id])
-  end
-
-  def new
-    @article = Article.new
-  end
-
+class CommentsController < ApplicationController
   def create
-    @article = Article.new(article_params)
-
-    if @article.save
-      redirect_to @article
-    else
-      render :new, status: :unprocessable_entity
-    end
-  end
-
-  def edit
-    @article = Article.find(params[:id])
-  end
-
-  def update
-    @article = Article.find(params[:id])
-
-    if @article.update(article_params)
-      redirect_to @article
-    else
-      render :edit, status: :unprocessable_entity
-    end
+    @article = Article.find(params[:article_id])
+    @comment = @article.comments.create(comment_params)
+    redirect_to article_path(@article)
   end
 
   def destroy
-    @article = Article.find(params[:id])
-    @article.destroy
-
-    redirect_to root_path, status: :see_other
+    @article = Article.find(params[:article_id])
+    @comment = @article.comments.find(params[:id])
+    @comment.destroy
+    redirect_to article_path(@article), status: :see_other
   end
 
   private
-    def article_params
-      params.require(:article).permit(:title, :body :status)
+    def comment_params
+      params.require(:comment).permit(:commenter, :body, :status)
     end
 end
