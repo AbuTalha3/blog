493ef1777c6e7c0bfe92122d592cdf866a123ae1
AbuTalha3 <127955219+AbuTalha3@users.noreply.github.com>
2023-11-28 11:35:04 +0500

    Add security feature

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -1,19 +1,9 @@
-class CommentsController < ApplicationController
-  def create
-    @article = Article.find(params[:article_id])
-    @comment = @article.comments.create(comment_params)
-    redirect_to article_path(@article)
+class ArticlesController < ApplicationController
+  http_basic_authenticate_with name: "dhh", password: "secret", except: [:index, :show]
+
+  def index
+    @articles = Article.all
   end
 
-  def destroy
-    @article = Article.find(params[:article_id])
-    @comment = @article.comments.find(params[:id])
-    @comment.destroy
-    redirect_to article_path(@article), status: :see_other
-  end
-
-  private
-    def comment_params
-      params.require(:comment).permit(:commenter, :body, :status)
-    end
+  # snippet for brevity
 end
